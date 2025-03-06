3ff9a4d4373765350e24d514ae6c5472cd4d7435
bleedcloudVIII <yurikaras8@gmail.com>
2025-03-07 09:55:53 +1000

    Сделал табы(пространства)

---
+++ src/locations/render_locations.js
@@ -0,0 +1,36 @@
+const TabGroup = require("electron-tabs");
+
+// Select tab-group
+const tabGroup = document.querySelector("tab-group");
+
+// Setup the default tab which is created when the "New Tab" button is clicked
+tabGroup.setDefaultTab({
+    title: "New Page",
+    src: "./locations/new_location/new_location.html",
+    active: true
+});
+
+const TabsOptions = {
+    closable: false,
+}
+
+tabGroup.addTab({
+    ...TabsOptions,
+    title: "electron",
+    src: ""
+});
+tabGroup.addTab({
+    ...TabsOptions,
+    title: "oiuytr",
+    src: ""
+});
+tabGroup.addTab({
+    ...TabsOptions,
+    title: "876543q",
+    src: ""
+});
+tabGroup.addTab({
+    ...TabsOptions,
+    title: "thrtgefe",
+    src: "./locations/location.html"
+});
--- src/main.js
+++ src/main.js
@@ -2,59 +2,77 @@ const { app, BrowserWindow } = require('electron')
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const fs = require('fs');
+const electron = require('electron')
 
-const createWindow = () => {
+// const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
-    })
-
-    win.loadFile('./src/index.html')
-}
-
-function ad() {
-    const PROTO_PATH = __dirname + '/keeper.proto';
-
-    const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
-    });
-
-    const exampleProto = grpc.loadPackageDefinition(packageDefinition).keeper;
-    const client = new (exampleProto.FileService)('localhost:5000', grpc.credentials.createInsecure());
-
-    const metadata = new grpc.Metadata();
-    metadata.set("file_name", '223458.txt');
-    metadata.set("location", '3');
-
-    const fileStream = fs.createReadStream('./src/text.txt');
-
-    const call = client.save(metadata, (err, response) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(response);
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            webviewTag: true,
         }
     });
 
-    fileStream.on('data', (chunk) => {
-        console.log(chunk);
-        call.write({ bytes: chunk }); // Отправляем чанк
-    });
-
-    fileStream.on('end', () => {
-        call.end(); // Завершаем поток
-    });
-
-    fileStream.on('error', (err) => {
-        console.error('Ошибка при чтении файла:', err);
-    });
+    win.loadFile('src/index.html');
 }
 
-app.whenReady().then(() => {
-    createWindow()
-    ad()
-})
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+// function ad() {
+//     const PROTO_PATH = __dirname + '/keeper.proto';
+//
+//     const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+//         keepCase: true,
+//         longs: String,
+//         enums: String,
+//         defaults: true,
+//         oneofs: true
+//     });
+//
+//     const exampleProto = grpc.loadPackageDefinition(packageDefinition).keeper;
+//     const client = new (exampleProto.FileService)('localhost:5000', grpc.credentials.createInsecure());
+//
+//     const metadata = new grpc.Metadata();
+//     metadata.set("file_name", '223458.txt');
+//     metadata.set("location", '3');
+//
+//     const fileStream = fs.createReadStream('./src/text.txt');
+//
+//     const call = client.save(metadata, (err, response) => {
+//         if (err) {
+//             console.error(err);
+//         } else {
+//             console.log(response);
+//         }
+//     });
+//
+//     fileStream.on('data', (chunk) => {
+//         console.log(chunk);
+//         call.write({ bytes: chunk }); // Отправляем чанк
+//     });
+//
+//     fileStream.on('end', () => {
+//         call.end(); // Завершаем поток
+//     });
+//
+//     fileStream.on('error', (err) => {
+//         console.error('Ошибка при чтении файла:', err);
+//     });
+// }
+//
