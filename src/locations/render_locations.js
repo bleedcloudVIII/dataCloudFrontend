95df7560faa15a100016b82805df38e917cb5769
bleedcloudVIII <yurikaras8@gmail.com>
2025-03-07 11:10:16 +1000

    Переделал табы (пространства)

--- src/locations/render_locations.js
+++ src/locations/render_locations.js
@@ -1,40 +1,26 @@
 const TabGroup = require("electron-tabs");
 
-// Select tab-group
 const tabGroup = document.querySelector("tab-group");
 
-// Setup the default tab which is created when the "New Tab" button is clicked
 tabGroup.setDefaultTab({
-    title: "New Page",
+    title: "Новое пространство",
     src: "./locations/new_location/new_location.html",
     active: true,
 });
 
 const TabsOptions = {
     closable: false,
-    webviewAttributes: {
-        "height": "30px",
-    }
+    // webviewAttributes: {
+    //     "height": "30px",
+    // },
+    src: "./locations/location.html",
 }
 
-tabGroup.addTab({
-    ...TabsOptions,
-    title: "electron",
-    src: ""
-});
-tabGroup.addTab({
-    ...TabsOptions,
-    title: "oiuytr",
-    src: ""
-});
-tabGroup.addTab({
-    ...TabsOptions,
-    title: "876543q",
-    src: ""
-});
-tabGroup.addTab({
-    ...TabsOptions,
-    title: "thrtgefe",
-    src: "./locations/location.html",
+const tabs = ["Общее пространство", "Пространство1", "Моё", "Лето2020", "Анапа"];
 
+tabs.forEach(tab => {
+    tabGroup.addTab({
+        ...TabsOptions,
+        title: tab,
+    });
 });
