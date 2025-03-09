bf0bfe0cce35c67e391ca164fcfaa2734fa94046
bleedcloudVIII <yurikaras8@gmail.com>
2025-03-09 15:23:45 +1000

    Добавление файла

--- src/locations/render_files.js
+++ src/locations/render_files.js
@@ -1,5 +1,43 @@
 const files = ["Item 1", "Item 2", "Item 3", "Item 4"];
 
+
+function onMouseOver() {
+    this.src = './../static/file_download.svg';
+}
+
+function onMouseOut() {
+    this.src = './../static/file.svg';
+}
+
+function onFileClick(file) {
+    console.log(file);
+    console.log('aasdasaasc');
+}
+
+function addPlus(block) {
+    const file = document.createElement("div");
+    const image = document.createElement("img");
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+
+    input.id = "add";
+    input.style.display = "none";
+    input.type = "file";
+
+    label.textContent = "Добавить";
+    label.htmlFor = "add";
+    label.classList.add("label-add");
+
+    image.src = "./../static/file_plus.svg";
+    image.style.marginBottom = "5px";
+
+    file.appendChild(label)
+    label.appendChild(image);
+    file.appendChild(input)
+
+    block.appendChild(file);
+}
+
 function renderFiles(items) {
     const filesList = document.getElementById("files_block");
 
@@ -14,12 +52,18 @@ function renderFiles(items) {
         text.textContent = item;
 
         image.src = "./../static/file.svg";
-
+        image.onmouseover = onMouseOver;
+        image.onmouseout = onMouseOut;
+        image.onclick = function () {
+            onFileClick(item)
+        };
 
         file.appendChild(image)
         file.appendChild(text)
         filesList.appendChild(file);
     });
+
+    addPlus(filesList);
 }
 
 renderFiles(files);
